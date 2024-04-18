8529676b8254bb01307d1c8aec0fe53329eb2019
LiudmylaBykova <bykova.luda@gmail.com>
2024-04-18 18:24:12 +0300

    create repository, add hw

---
+++ vite-project/src/generics/1.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// Типізуйте асинхронну функцію fetchData, яка приймає URL ресурсу та повертає об'єкт з даними.
+// Використовуйте Generics для типізації повернутих даних.
+
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
+
+fetchData;
